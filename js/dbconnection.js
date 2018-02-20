38b72e3c7ca66a5aea1446a8c3165d3193c3086f
Douglas Rodrigues Pinto Neto <douglasrodrigues@brq.com>
2018-02-20 11:44:32 -0300

    make it better

--- js/dbconnection.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var dbconnect = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'aaaa',
-	database: 'catalogo'
-});
-
-module.exports = dbconnect;
